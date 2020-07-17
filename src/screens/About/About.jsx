b32863dccb1e69b77800ae9050568908bb775ecc
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2020-07-17 15:21:53 -0400

    about page complete

--- src/screens/About/About.jsx
+++ src/screens/About/About.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import './About.scss'
 
 export default function About() {
     return (
-        <div>
-            this is the about page
+        <div className='about-container' id='about'>
+            <section class="hero is-medium is-primary is-bold">
+                <div class="hero-body">
+                    <div class="container">
+                    <h1 class="title">
+                        Search for your favorite skilled professional, based on location, day and time.
+                    </h1>
+                    <h2 class="subtitle">
+                        WhichDayZ is a mobile application which taps into your favorite skilled professional schedule such as barbers, baristas, hair stylists, etc.<br></br>Their flexibility can vary from different venues and time frames.<br></br> WhichDayZ allows you to follow skilled profesionals of your desire so there is no confusion on when and what time to go visit
+                    </h2>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -3,7 +3,7 @@ import './Home.scss'
 
 export default function Home() {
     return (
-        <section className='hero is-fullheight'>
+        <section className='hero is-fullheight' id='home'>
             <div className="home-container">
                   <h1 className="subtitle is-uppercase has-text-centered has-text-primary is-size-1">our new site is</h1>
             <h1 className="title is-uppercase has-text-centered has-text-primary is-size-1">coming soon</h1>
