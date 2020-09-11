78ba64afad409e7643ae27d3abc6e3343c33cdf0
Andrew <andrewk745@gmail.com>
2020-09-11 12:07:52 -0400

    auth is completely functional

--- src/screens/Firebase/FirebaseSignIn.jsx
+++ src/screens/Firebase/FirebaseSignIn.jsx
@@ -24,7 +24,7 @@ export default class FirebaseSignIn extends Component {
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       firebase.auth.FacebookAuthProvider.PROVIDER_ID,
       firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      // firebase.auth.GithubAuthProvider.PROVIDER_ID,
       firebase.auth.EmailAuthProvider.PROVIDER_ID
     ],
     callbacks: {
