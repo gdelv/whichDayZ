0b192ed826e0f8b780d6424cfb8ad5e149eb9715
Andrew <andrewk745@gmail.com>
2020-09-14 16:39:56 -0400

    set up home component with renderText function

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.scss'
 import '../Home/Carousel/FadeCarousel'
-import { FadeCarousel }  from '../Home/Carousel/FadeCarousel'
+import { FadeCarousel } from '../Home/Carousel/FadeCarousel'
 
 export default function Home() {
+
+    const [hovered, isHovered] = useState(false);
+
+    renderText = () => {
+        if (hovered) {
+            return (
+                <div className="home-container">
+                    <h1 className="subtitle is-uppercase has-text-centered has-text-primary is-size-1">our new site is</h1>
+                    <h1 className="title is-uppercase has-text-centered has-text-primary is-size-1">coming soon</h1>
+                    <h1 className="subtitle is-uppercase has-text-centered has-text-primary is-size-1">stay tuned!</h1>
+                </div>
+            )
+        }
+    }
+
     return (
         <>
-        <FadeCarousel/>
+            <FadeCarousel
+                onMouseEnter={() => isHovered(true)}
+                onMouseLeave={() => isHovered(false)}
+                renderText={renderText()}
+            />
         </>
         // <section className='hero is-fullheight' id='home'>
         //     <div className="home-container">
